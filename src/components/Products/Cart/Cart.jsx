e1f136e6b9c2f46e1d5458208071b0321d449ed9
IDucuara <97700692+IDucuara@users.noreply.github.com>
2022-09-30 13:15:02 -0500

    Updating Pufi app

--- src/components/Products/Cart/Cart.jsx
+++ src/components/Products/Cart/Cart.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+import NavBar from '../../NavBar/NavBar'
+import style from '../../Products/Product.module.css'
+import banner from '../../../images/banner.jpg'
+import image7 from '../../../images/cart2.png'
 
 export default function Cart() {
   return (
-    <div>Cart</div>
+    <Container fluid className={style.container}>
+      <Row>
+        <NavBar />
+        <img src={banner} alt='decoration image'  />
+      </Row>
+      <Row >
+        <Col>
+        <div className={style.productImg}><img src={image7} alt='cart bag ' /></div>
+        </Col>
+        <Col className={style.product}>
+          <h1 >Pufi CART</h1>
+          <h4>Descripción de este producto.</h4>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet nemo alias assumenda, exercitationem est quia praesentium minus autem rem accusamus sequi fugiat sunt expedita maxime, officia iste dolores eius cumque?</p>
+        </Col>
+      </Row>
+
+
+    </Container>
   )
 }
--- src/components/Products/Puff/Puff.jsx
+++ src/components/Products/Puff/Puff.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+import NavBar from '../../NavBar/NavBar'
+import style from '../../Products/Product.module.css'
+import banner from '../../../images/banner.jpg'
+import image6 from '../../../images/puff2.png'
 
 export default function Puff() {
   return (
-    <div>Puff</div>
+    <Container fluid className={style.container}>
+      <Row>
+        <NavBar />
+        <img src={banner} alt='decoration image'  />
+      </Row>
+      <Row >
+        <Col>
+        <div className={style.productImg}><img src={image6} alt='puff outdoor' /></div>
+        </Col>
+        <Col className={style.product}>
+          <h1 >Pufi PUFF</h1>
+          <h4>Descripción de este producto.</h4>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci vero dicta vitae omnis reiciendis et quod veritatis architecto. Facilis, tempore odio Rem aperiam blanditiis cupiditate perferendis provident temporibus. Suscipit, odit</p>
+        </Col>
+      </Row>
+
+
+    </Container>
   )
 }
